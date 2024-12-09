c6efc83bb24859579359213942374f5e6e58e658
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2024-12-09 23:02:38 +0330

    inputFields and handleAdd functions

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,32 @@
+import { FC, useState } from "react";
 import "./App.css";
+import InputFields from "./components/inputFields";
+import { Todo } from "./model";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
   return (
-    <>
+    <div className="min-w-[380px] max-w-2xl flex flex-col justify-center items-center gap-5">
       <h1 className="text-blue-600">hello</h1>
-    </>
+      <InputFields todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.todo}</li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ vite-project/src/components/inputFields.tsx
@@ -0,0 +1,23 @@
+import { Button, Input, Space } from "antd";
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+const InputFields: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  return (
+    <Space.Compact>
+      <form onSubmit={handleAdd}>
+        <Input
+          placeholder="type something..."
+          className="w-[350px]"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </Space.Compact>
+  );
+};
+
+export default InputFields;
